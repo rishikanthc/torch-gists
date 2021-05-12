9e8014acf75e95ec2122500e1f3559f545359967
rishikanth <r3chandr@ucsd.edu>
2021-05-12 01:44:39 +0000

    addiing gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="torch-gists", # Replace with your own username
-    version="0.0.0.1",
+    version="0.0.0.2",
     author="Rishikanth",
     author_email="r3chandr@ucsd.edu",
     description="A collection of models and utilities for pytorch",
